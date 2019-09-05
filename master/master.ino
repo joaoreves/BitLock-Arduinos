3aa3f7ee13cafdc739df5cfd1f81dc4b5f66c5db
joaoreves <joreves@icloud.com>
2019-09-05 17:52:29 +0100

    encription working

--- master/master.ino
+++ master/master.ino
@@ -5,12 +5,26 @@ char receive_msg[RH_ASK_MAX_MESSAGE_LEN];
 char decript_msg[RH_ASK_MAX_MESSAGE_LEN];
 char encript_msg[RH_ASK_MAX_MESSAGE_LEN];
 const char msg[] = "abre de cesamo";
+long decript_index;
+
+char key[][50] =
+{
+    "nj098anw12n12jn4jndnklenw24325jopji320ave93u2nlbkn",
+    "janfnbwi23470801in3wq23nbfwq12424jnjnini0032r80ian",
+    "zxy7njsaoj21klwnqmklfqw4alnqwnndaho28niuh328nwbeh2",
+    "12kjnndsviwonnwopjnf12j48932b89219001injjn1j2ej2jn",
+    "43646bsjhbhjbnkwh8912r1ub80089sdhasj09wss7fdvs7vs7",
+    "ksn212nnsl24m5sklm24slknlkni2n3inr23oiooi1ni0nofin",
+    "o3290ikmmkknin21490sdnanijiowaj09isnifjasnifowenfo",
+    "nfjj9wqjiwh0qhrqh0hh121e2142i1h210h21h12h1h1h14h5h"
+};
 
 RH_ASK driver;
 
 void setup()
 {
     Serial.begin(9600);
+    randomSeed(analogRead(0));
     if (!driver.init())
          Serial.println("init failed");
 
@@ -21,11 +35,10 @@ void loop()
 {
     if(digitalRead(2) == HIGH)
     {
-        encript();
         send();
     }
 
-    delay(500);
+    delay(1500);
 }
 
 void receive()
@@ -49,9 +62,10 @@ void receive()
 
 void decript()
 {
-    for (int i = 0; receive_msg[i] != '\0'; i++)
+    decript_index = receive_msg[0];
+    for (int i = 0; receive_msg[i + 1] != '\0'; i++)
     {
-        decript_msg[i] = receive_msg[i] - 3;
+        decript_msg[i] = receive_msg[i + 1] - key[decript_index][i];
     }
     Serial.print("Receive: ");
     Serial.println(decript_msg);
@@ -59,16 +73,24 @@ void decript()
 
 void encript()
 {
-    int i = 0;
-    for (i; i < sizeof(msg) - 1; i++)
+    decript_index = random(0, 7);
+    for (int i = 0; i < sizeof(msg) - 1; i++)
     {
-        encript_msg[i] = msg[i] + 3;
+        encript_msg[i] = msg[i] + key[decript_index][i];
     }
 }
 
 void send()
 {
-    driver.send((uint8_t *)encript_msg, strlen(encript_msg));
+    encript();
+    char final_msg[50];
+    final_msg[0] = decript_index;
+    for (int i = 1; i < sizeof(encript_msg); i++)
+    {
+        final_msg[i] = encript_msg[i-1];
+    }
+
+    driver.send((uint8_t *)final_msg, strlen(final_msg));
     driver.waitPacketSent();
     delay(1000);
 }
--- slave/slave.ino
+++ slave/slave.ino
@@ -7,15 +7,29 @@ char decript_msg[RH_ASK_MAX_MESSAGE_LEN];
 const char *msg = "aberto";
 const char *msg1 = "fechado";
 String palavra = "abre de cesamo";
+long decript_index;
+
+char key[][50] =
+{
+    "nj098anw12n12jn4jndnklenw24325jopji320ave93u2nlbkn",
+    "janfnbwi23470801in3wq23nbfwq12424jnjnini0032r80ian",
+    "zxy7njsaoj21klwnqmklfqw4alnqwnndaho28niuh328nwbeh2",
+    "12kjnndsviwonnwopjnf12j48932b89219001injjn1j2ej2jn",
+    "43646bsjhbhjbnkwh8912r1ub80089sdhasj09wss7fdvs7vs7",
+    "ksn212nnsl24m5sklm24slknlkni2n3inr23oiooi1ni0nofin",
+    "o3290ikmmkknin21490sdnanijiowaj09isnifjasnifowenfo",
+    "nfjj9wqjiwh0qhrqh0hh121e2142i1h210h21h12h1h1h14h5h"
+};
 
 RH_ASK driver;
 
 void setup()
 {
     Serial.begin(9600);
+    randomSeed(analogRead(0));
     if (!driver.init())
          Serial.println("init failed");
-         
+
     pinMode(LED_BUILTIN,OUTPUT);
     digitalWrite(LED_BUILTIN,LOW);
     pinMode(3,OUTPUT);
@@ -46,18 +60,19 @@ void loop()
 
 void encript()
 {
-    int i = 0;
-    for (i; i < sizeof(msg) - 1; i++)
+    decript_index = random(0, 7);
+    for (int i = 0; i < sizeof(msg) - 1; i++)
     {
-        encript_msg[i] = msg[i] + 3;
+        encript_msg[i] = msg[i] + key[decript_index][i];
     }
 }
 
 void decript()
 {
-    for (int i = 0; receive_msg[i] != '\0'; i++)
+    decript_index = receive_msg[0];
+    for (int i = 0; receive_msg[i + 1] != '\0'; i++)
     {
-        decript_msg[i] = receive_msg[i] - 3;
+        decript_msg[i] = receive_msg[i + 1] - key[decript_index][i];
     }
     Serial.print("Receive: ");
     Serial.println(decript_msg);
@@ -84,7 +99,14 @@ void receive()
 
 void send()
 {
-    driver.send((uint8_t *)encript_msg, strlen(encript_msg));
+    char final_msg[50];
+    final_msg[0] = decript_index;
+    for (int i = 1; i < sizeof(encript_msg); i++)
+    {
+        final_msg[i] = encript_msg[i-1];
+    }
+
+    driver.send((uint8_t *)final_msg, strlen(final_msg));
     driver.waitPacketSent();
     delay(1000);
 }
