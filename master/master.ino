f75c1fb854bb6cfd759b06658fcf92e61822301e
joaoreves <joreves@icloud.com>
2019-09-06 15:54:56 +0100

    nao envia e recebe simultaneamente

--- master/master.ino
+++ master/master.ino
@@ -4,19 +4,20 @@
 char receive_msg[RH_ASK_MAX_MESSAGE_LEN];
 char decript_msg[RH_ASK_MAX_MESSAGE_LEN];
 char encript_msg[RH_ASK_MAX_MESSAGE_LEN];
-const char msg[] = "abre de cesamo";
 long decript_index;
+String open = "aberto";
+String closed = "fechado";
 
-char key[][50] =
+char key[][52] =
 {
-    "nj098anw12n12jn4jndnklenw24325jopji320ave93u2nlbkn",
-    "janfnbwi23470801in3wq23nbfwq12424jnjnini0032r80ian",
-    "zxy7njsaoj21klwnqmklfqw4alnqwnndaho28niuh328nwbeh2",
-    "12kjnndsviwonnwopjnf12j48932b89219001injjn1j2ej2jn",
-    "43646bsjhbhjbnkwh8912r1ub80089sdhasj09wss7fdvs7vs7",
-    "ksn212nnsl24m5sklm24slknlkni2n3inr23oiooi1ni0nofin",
-    "o3290ikmmkknin21490sdnanijiowaj09isnifjasnifowenfo",
-    "nfjj9wqjiwh0qhrqh0hh121e2142i1h210h21h12h1h1h14h5h"
+    ".nj098anw12n12jn4jndnklenw24325jopji320ave93u2nlbkn",
+    "-janfnbwi23470801in3wq23nbfwq12424jnjnini0032r80ian",
+    "_zxy7njsaoj21klwnqmklfqw4alnqwnndaho28niuh328nwbeh2",
+    "(12kjnndsviwonnwopjnf12j48932b89219001injjn1j2ej2jn",
+    ")43646bsjhbhjbnkwh8912r1ub80089sdhasj09wss7fdvs7vs7",
+    "{ksn212nnsl24m5sklm24slknlkni2n3inr23oiooi1ni0nofin",
+    "}o3290ikmmkknin21490sdnanijiowaj09isnifjasnifowenfo",
+    "+nfjj9wqjiwh0qhrqh0hh121e2142i1h210h21h12h1h1h14h5h"
 };
 
 RH_ASK driver;
@@ -29,13 +30,33 @@ void setup()
          Serial.println("init failed");
 
     pinMode(2, INPUT);
+    pinMode(LED_BUILTIN, OUTPUT);
 }
 
 void loop()
 {
+    receive();
+    Serial.println(decript_msg);
+    if (open.equals(decript_msg))
+    {
+        digitalWrite(LED_BUILTIN,HIGH);
+        decript_msg[0] = '\0';
+        Serial.println("aberto");
+        receive();
+    }
+    if (closed.equals(decript_msg))
+    {
+        digitalWrite(LED_BUILTIN,LOW);
+        decript_msg[0] = '\0';
+        Serial.println("fechado");
+    }
+
+    delay(2000);
+
     if(digitalRead(2) == HIGH)
     {
-        send();
+        Serial.println("A enviar pass de abertura...");
+        send("abre de cesamo");
     }
 
     delay(1500);
@@ -53,36 +74,34 @@ void receive()
         {
             receive_msg[i] = buff[i];
         }
-        receive_msg[i++] = '\0';
-        Serial.print("receive: ");
-        Serial.println(receive_msg);
+        receive_msg[i] = '\0';
         decript();
     }
 }
 
 void decript()
 {
-    decript_index = receive_msg[0];
+    decript_index = receive_msg[0] - '0';
     for (int i = 0; receive_msg[i + 1] != '\0'; i++)
     {
         decript_msg[i] = receive_msg[i + 1] - key[decript_index][i];
+        decript_msg[i+1] = '\0';
     }
-    Serial.print("Receive: ");
-    Serial.println(decript_msg);
 }
 
-void encript()
+void encript(char msg[])
 {
     decript_index = random(0, 7);
-    for (int i = 0; i < sizeof(msg) - 1; i++)
+    for (int i = 0; msg[i]; i++)
     {
         encript_msg[i] = msg[i] + key[decript_index][i];
+        encript_msg[i + 1] = '\0';
     }
 }
 
-void send()
+void send(char msg[])
 {
-    encript();
+    encript(msg);
     char final_msg[50];
     final_msg[0] = decript_index;
     for (int i = 1; i < sizeof(encript_msg); i++)
--- slave/slave.ino
+++ slave/slave.ino
@@ -4,21 +4,19 @@
 char encript_msg[RH_ASK_MAX_MESSAGE_LEN];
 char receive_msg[RH_ASK_MAX_MESSAGE_LEN];
 char decript_msg[RH_ASK_MAX_MESSAGE_LEN];
-const char *msg = "aberto";
-const char *msg1 = "fechado";
 String palavra = "abre de cesamo";
 long decript_index;
 
-char key[][50] =
+char key[][52] =
 {
-    "nj098anw12n12jn4jndnklenw24325jopji320ave93u2nlbkn",
-    "janfnbwi23470801in3wq23nbfwq12424jnjnini0032r80ian",
-    "zxy7njsaoj21klwnqmklfqw4alnqwnndaho28niuh328nwbeh2",
-    "12kjnndsviwonnwopjnf12j48932b89219001injjn1j2ej2jn",
-    "43646bsjhbhjbnkwh8912r1ub80089sdhasj09wss7fdvs7vs7",
-    "ksn212nnsl24m5sklm24slknlkni2n3inr23oiooi1ni0nofin",
-    "o3290ikmmkknin21490sdnanijiowaj09isnifjasnifowenfo",
-    "nfjj9wqjiwh0qhrqh0hh121e2142i1h210h21h12h1h1h14h5h"
+    ".nj098anw12n12jn4jndnklenw24325jopji320ave93u2nlbkn",
+    "-janfnbwi23470801in3wq23nbfwq12424jnjnini0032r80ian",
+    "_zxy7njsaoj21klwnqmklfqw4alnqwnndaho28niuh328nwbeh2",
+    "(12kjnndsviwonnwopjnf12j48932b89219001injjn1j2ej2jn",
+    ")43646bsjhbhjbnkwh8912r1ub80089sdhasj09wss7fdvs7vs7",
+    "{ksn212nnsl24m5sklm24slknlkni2n3inr23oiooi1ni0nofin",
+    "}o3290ikmmkknin21490sdnanijiowaj09isnifjasnifowenfo",
+    "+nfjj9wqjiwh0qhrqh0hh121e2142i1h210h21h12h1h1h14h5h"
 };
 
 RH_ASK driver;
@@ -27,18 +25,36 @@ void setup()
 {
     Serial.begin(9600);
     randomSeed(analogRead(0));
+
     if (!driver.init())
          Serial.println("init failed");
-
+         
     pinMode(LED_BUILTIN,OUTPUT);
     digitalWrite(LED_BUILTIN,LOW);
     pinMode(3,OUTPUT);
-    //pinMode(4,INPUT);
+    pinMode(4,INPUT);
 }
 
 void loop()
 {
+    //driver.setModeTx();
+    
+    if (digitalRead(4) == HIGH)
+    {
+        Serial.println("aberto");
+        send("aberto");
+    }
+    else
+    {
+        Serial.println("fechado");
+        //send("fechado");
+    }
+    delay(5000);
+    
+    driver.setModeRx();
+    
     receive();
+    Serial.println(decript_msg);
     if (palavra.equals(decript_msg))
     {
         digitalWrite(LED_BUILTIN,HIGH);
@@ -50,20 +66,16 @@ void loop()
         digitalWrite(LED_BUILTIN,LOW);
         digitalWrite(3, LOW);
     }
-
-    /*if (digitalRead(4) == HIGH)
-    {
-
-    }*/
-    delay(500);
+    delay(5000);
 }
 
-void encript()
+void encript(char msg[])
 {
     decript_index = random(0, 7);
-    for (int i = 0; i < sizeof(msg) - 1; i++)
+    for (int i = 0; msg[i]; i++)
     {
         encript_msg[i] = msg[i] + key[decript_index][i];
+        encript_msg[i + 1] = '\0';
     }
 }
 
@@ -73,9 +85,8 @@ void decript()
     for (int i = 0; receive_msg[i + 1] != '\0'; i++)
     {
         decript_msg[i] = receive_msg[i + 1] - key[decript_index][i];
+        decript_msg[i+1] = '\0';
     }
-    Serial.print("Receive: ");
-    Serial.println(decript_msg);
 }
 
 void receive()
@@ -91,17 +102,16 @@ void receive()
             receive_msg[i] = buff[i];
         }
         receive_msg[i++] = '\0';
-        Serial.print("receive: ");
-        Serial.println(receive_msg);
         decript();
     }
 }
 
-void send()
+void send(char msg[])
 {
+    encript(msg);
     char final_msg[50];
-    final_msg[0] = decript_index;
-    for (int i = 1; i < sizeof(encript_msg); i++)
+    final_msg[0] = '0' + decript_index;
+    for (int i = 1; i < sizeof(encript_msg) ; i++)
     {
         final_msg[i] = encript_msg[i-1];
     }
